385aa24e6070832c0354795754d0a7c8fa6f00ca
RicardoLeitao <ricardo.l.1992@gmail.com>
2014-04-08 16:49:57 +0100

    Form for master server

--- MasterServer/Form1.Designer.cs
+++ MasterServer/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Log = new System.Windows.Forms.TextBox();
+            this.LogLabel = new System.Windows.Forms.Label();
+            this.ServerLabel = new System.Windows.Forms.Label();
+            this.PadIntLocationLabel = new System.Windows.Forms.Label();
+            this.ServerLocationLoad = new System.Windows.Forms.ListView();
+            this.ServerLocation = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ServerLoad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PadIntLocation = new System.Windows.Forms.ListView();
+            this.PadIntID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Location = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(12, 42);
+            this.Log.Multiline = true;
+            this.Log.Name = "Log";
+            this.Log.ReadOnly = true;
+            this.Log.Size = new System.Drawing.Size(442, 480);
+            this.Log.TabIndex = 0;
+            // 
+            // LogLabel
+            // 
+            this.LogLabel.AutoSize = true;
+            this.LogLabel.Location = new System.Drawing.Point(13, 23);
+            this.LogLabel.Name = "LogLabel";
+            this.LogLabel.Size = new System.Drawing.Size(28, 13);
+            this.LogLabel.TabIndex = 3;
+            this.LogLabel.Text = "Log:";
+            // 
+            // ServerLabel
+            // 
+            this.ServerLabel.AutoSize = true;
+            this.ServerLabel.Location = new System.Drawing.Point(490, 23);
+            this.ServerLabel.Name = "ServerLabel";
+            this.ServerLabel.Size = new System.Drawing.Size(95, 13);
+            this.ServerLabel.TabIndex = 4;
+            this.ServerLabel.Text = "Servers Locations:";
+            // 
+            // PadIntLocationLabel
+            // 
+            this.PadIntLocationLabel.AutoSize = true;
+            this.PadIntLocationLabel.Location = new System.Drawing.Point(490, 200);
+            this.PadIntLocationLabel.Name = "PadIntLocationLabel";
+            this.PadIntLocationLabel.Size = new System.Drawing.Size(90, 13);
+            this.PadIntLocationLabel.TabIndex = 5;
+            this.PadIntLocationLabel.Text = "PadInt Locations:";
+            // 
+            // ServerLocationLoad
+            // 
+            this.ServerLocationLoad.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ServerLocation,
+            this.ServerLoad});
+            this.ServerLocationLoad.Location = new System.Drawing.Point(490, 42);
+            this.ServerLocationLoad.Name = "ServerLocationLoad";
+            this.ServerLocationLoad.Size = new System.Drawing.Size(270, 136);
+            this.ServerLocationLoad.TabIndex = 6;
+            this.ServerLocationLoad.UseCompatibleStateImageBehavior = false;
+            this.ServerLocationLoad.View = System.Windows.Forms.View.Details;
+            // 
+            // ServerLocation
+            // 
+            this.ServerLocation.Text = "Server Location";
+            this.ServerLocation.Width = 192;
+            // 
+            // ServerLoad
+            // 
+            this.ServerLoad.Text = "Server Load";
+            this.ServerLoad.Width = 72;
+            // 
+            // PadIntLocation
+            // 
+            this.PadIntLocation.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PadIntID,
+            this.Location});
+            this.PadIntLocation.Location = new System.Drawing.Point(490, 217);
+            this.PadIntLocation.Name = "PadIntLocation";
+            this.PadIntLocation.Size = new System.Drawing.Size(270, 305);
+            this.PadIntLocation.TabIndex = 7;
+            this.PadIntLocation.UseCompatibleStateImageBehavior = false;
+            this.PadIntLocation.View = System.Windows.Forms.View.Details;
+            // 
+            // PadIntID
+            // 
+            this.PadIntID.Text = "PadInt ID";
+            // 
+            // Location
+            // 
+            this.Location.Text = "Location";
+            this.Location.Width = 205;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 532);
+            this.Controls.Add(this.PadIntLocation);
+            this.Controls.Add(this.ServerLocationLoad);
+            this.Controls.Add(this.PadIntLocationLabel);
+            this.Controls.Add(this.ServerLabel);
+            this.Controls.Add(this.LogLabel);
+            this.Controls.Add(this.Log);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Log;
+        private System.Windows.Forms.Label LogLabel;
+        private System.Windows.Forms.Label ServerLabel;
+        private System.Windows.Forms.Label PadIntLocationLabel;
+        private System.Windows.Forms.ListView ServerLocationLoad;
+        private System.Windows.Forms.ColumnHeader ServerLocation;
+        private System.Windows.Forms.ColumnHeader ServerLoad;
+        private System.Windows.Forms.ListView PadIntLocation;
+        private System.Windows.Forms.ColumnHeader PadIntID;
+        private System.Windows.Forms.ColumnHeader Location;
     }
 }
 
--- MasterServer/Form1.cs
+++ MasterServer/Form1.cs
@@ -4,17 +4,64 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MasterServer
 {
+    delegate void delLog(string logEntry);
+    delegate void delServerLoad(Dictionary<string, int> serversLoad);
+    delegate void delUpdatePadInt(Dictionary<int, string> padIntLocation);
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            TcpChannel channel = new TcpChannel(8086);
+            ChannelServices.RegisterChannel(channel, true);
+            RemoteMaster master = new RemoteMaster(this);
+            string start = "Starting Master Server on port: " + 8086;
+            log(start);
+
+            RemotingServices.Marshal(
+                master,
+                "RemoteMaster",
+                typeof(RemoteMaster));
+            string end = "Master Server started";
+            log(end);
+        }
+
+
+        public void log(string logEntry)
+        {
+            Log.Text += logEntry + "\r\n";
+        }
+
+        public void updateServerLoad(Dictionary<string, int> serversLoad)
+        {
+            this.ServerLocationLoad.Items.Clear();
+            foreach (KeyValuePair<string, int> server in serversLoad)
+            {
+                string[] values = {server.Key, Convert.ToString(server.Value)};
+                var listItem = new ListViewItem(values);
+                this.ServerLocationLoad.Items.Add(listItem);
+            }
+        }
+
+        public void updatePadInts(Dictionary<int, string> padIntLocation)
+        {
+            this.PadIntLocation.Items.Clear();
+            foreach (KeyValuePair<int, string> padint in padIntLocation)
+            {
+                string[] padints = { Convert.ToString(padint.Key), padint.Value };
+                var listItem = new ListViewItem(padints);
+                this.PadIntLocation.Items.Add(listItem);
+            }
         }
     }
 }
